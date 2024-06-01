6c49c25dd6a889ae26d60638c156f84e730c8e7b
marvin.alivio <marvin.alivio@gmail.com>
2024-06-02 01:21:09 +0800

    Feat: adding js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let menu = document.getElementById("menu");
+let closeNav = document.getElementById("closeNav");
+function openNav(){
+    menu.style.top = "0";
+    closeNav.style.display = "inline-block"
+}
+
+function closeNow(){
+    menu.style.top = "-100vh"
+    closeNav.style.display = "none"
+}
+
+function closeNo(){
+    menu.style.top = "-100vh"
+    closeNav.style.display = "none"
+}
