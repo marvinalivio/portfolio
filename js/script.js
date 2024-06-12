39973468c850886aab1ae742f7d4a6fd43024876
marvin.alivio <marvin.alivio@gmail.com>
2024-06-12 20:40:28 +0800

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+window.addEventListener('scroll', function() { 
+    let scrollPosition = window.scrollY; 
+    let windowWidth = window.innerWidth; 
+    let element = document.querySelector('body'); 
+    if (windowWidth >= 768 && scrollPosition > 0) { 
+        element.classList.add('scrolled');
+        } 
+        else {
+             element.classList.remove('scrolled'); 
+        } 
+        
+}); 
+
+let animateClass = document.getElementById('aboutMe');
+
+window.onscroll = function(){
+    getAnimate(animateClass);
+}
+
+function getAnimate(animateClass){
+    
+    if (window.scrollY === 0 && animateClass === window.scrollY){
+        animateClass.classList.add("goAnimate")
+    }
+    else {
+        animateClass.classList.remove("goAnimate")
+    }
+}
+
+
 let menu = document.getElementById("menu");
 let closeNav = document.getElementById("closeNav");
 function openNav(){
@@ -13,4 +43,4 @@ function closeNow(){
 function closeNo(){
     menu.style.top = "-100vh"
     closeNav.style.display = "none"
-}
+};
